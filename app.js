4df1d523e51c7fb4606d7897aa14998186b23a82
jacando.tos <sereicikas.t@gmail.com>
2020-10-19 19:09:58 +0200

    add todo logic

---
+++ app.js
@@ -0,0 +1,45 @@
+const form = document.querySelector(".form");
+const input = document.querySelector(".form__input");
+const todoList = document.querySelector(".todo-list");
+
+const todoBlueprint = (text) => `
+  <li class="todo">
+    <p class="todo__text">${text}</p>
+    <button class="complete">
+      <svg>
+        <use xlink:href="img/sprite.svg#check-square"></use>
+      </svg>
+    </button>
+    <button class="delete">
+      <svg>
+        <use xlink:href="img/sprite.svg#trash-can"></use>
+      </svg>
+    </button>
+  </li>
+`;
+
+form.addEventListener("submit", addTodo);
+todoList.addEventListener("click", todoClick);
+
+function addTodo(event) {
+  event.preventDefault();
+  const inputValue = event.target[0].value;
+
+  if (!inputValue) {
+    alert("Please provide a todo name below.");
+    return;
+  }
+
+  todoList.insertAdjacentHTML("beforeend", todoBlueprint(inputValue));
+  input.value = "";
+}
+
+function todoClick(event){
+  const item = event.target;
+  
+  const completeButton = item.closest(".complete");
+  const deleteButton = item.closest(".delete");
+
+  if(completeButton) completeButton.parentElement.classList.toggle("completed");
+  if(deleteButton) deleteButton.parentElement.remove();
+}
